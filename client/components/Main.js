fb08112b9f899bb63e8ff89e9893b836e7451b50
Abdelrahman Serag <abdo.serag90@gmail.com>
2019-02-14 11:23:25 +0200

    adding photo component, building main page grid

--- client/components/Main.js
+++ client/components/Main.js
@@ -3,16 +3,16 @@ import { Link } from 'react-router';
 
 
 const Main = React.createClass({
-    render() {
-        return (
-            <div>
-                <h1>
-                    <Link to="/">Reduxstagram</Link>
-                </h1>
-                {React.cloneElement(this.props.children, this.props)}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h1>
+          <Link to="/">Reduxstagram</Link>
+          {React.cloneElement(this.props.children, this.props)}
+        </h1>
+      </div>
+    )
+  }
 })
 
 export default Main;
